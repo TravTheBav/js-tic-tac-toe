2271f52b383008d9742e86dad60b81ddabb007e7
TravTheBav <tzito93@gmail.com>
2023-08-12 13:48:09 -0400

    Display updates on player click

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// factory pattern
+const playerFactory = ((name, symbol) => {
+    return { name, symbol };
+});
+
 // module pattern
 const gameBoard = (() => {
     let board = [
-                [' ', ' ', ' '],
-                [' ', ' ', ' '],
-                [' ', ' ', ' ']
+                [null, null, null],
+                [null, null, null],
+                [null, null, null]
                 ]
 
     const updatePos = (row, col, symbol) => {
@@ -13,37 +18,69 @@ const gameBoard = (() => {
 })();
 
 // module pattern
-const displayController = ((board, game) => {
-    const setupBoxClickListeners = () => {
-        for (row = 0; row < board.length; row++) {
-            for (col = 0; col < board.length; col++) {
-                let displayBox = document.querySelector(`div[data-row="${row}"]`).querySelector(`[data-col="${col}"]`);
-                    displayBox.addEventListener('click', () => {
-                        symbol = displayBox.firstChild;
-                        // TO DO
-                        // make a game object that keeps track of game state
-                        // to start, a game should have alternating players that can be accessed
-                        if (symbol == ' ') {
-                            board.updatePos(row, col, game.getCurrentPlayer.symbol);
-                        }
-                    })           
+const displayController = (() => {
+    const setListeners = function () {
+        for (let row = 0; row < gameBoard.board.length; row++) {
+            for (let col = 0; col < gameBoard.board.length; col++) {
+                let box = _getDisplayBox(row, col);
+                    box.addEventListener('click', _boxClicked.bind(this, row, col), false);
+            }
+        }
+    }    
+
+    const renderBoard = function () {
+        for (row = 0; row < gameBoard.board.length; row++) {
+            for (col = 0; col < gameBoard.board.length; col++) {
+                let symbol = _getDisplayBox(row, col).firstChild;
+                symbol.innerHTML = gameBoard.board[row][col];                
             }
         }
     }
 
-    const renderBoard = () => {
-        for (row = 0; row < board.length; row++) {
-            for (col = 0; col < board.length; col++) {
-                let symbol = document.querySelector(`div[data-row="${row}"]`).querySelector(`[data-col="${col}"]`).firstChild;
-                symbol.innerHTML = board[row][col];                
-            }
+    const _getDisplayBox = function (row, col) {
+        return document.querySelector(`div[data-row="${row}"]`).querySelector(`[data-col="${col}"]`);
+    }
+
+    const _boxClicked = function (row, col) {
+        let box = _getDisplayBox(row, col);
+        let symbol = box.firstChild;
+        if (!symbol.innerHTML) {
+            gameBoard.updatePos(row, col, Game.getCurrentSymbol());            
+            Game.playerClick();
         }
     }
-    return { renderBoard }
-})(gameBoard.board);
 
-// factory pattern
-const playerFactory = ((name, symbol) => {
-    return { name, symbol };
-});
+    return { renderBoard, setListeners }
+})();
 
+// module pattern
+const Game = (function () {
+    let gameOver = false;
+    const playerOne = playerFactory(prompt('Enter player 1 name: '), 'X');
+    const playerTwo = playerFactory(prompt('Enter player 2 name: '), 'O');
+    let currentPlayer = playerOne;
+
+    const playerClick = function () {
+        if (!gameOver) {
+            displayController.renderBoard();
+            _toggleCurrentPlayer();
+        }
+    }
+
+    const _toggleCurrentPlayer = function () {
+        console.log(currentPlayer);
+        if (currentPlayer == playerOne) {
+            currentPlayer = playerTwo;
+        }   else  {
+            currentPlayer = playerOne;
+        }
+    }
+
+    const getCurrentSymbol = function () {
+        return currentPlayer.symbol;
+    }
+
+    return { getCurrentSymbol, playerClick }    
+})();
+
+displayController.setListeners();
